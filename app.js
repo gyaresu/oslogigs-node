fb2a809ece678afda21e24cf0fed89c7b776c4dc
Gareth <me@gareth.com.au>
2012-04-28 11:37:55 +0530

    changed port var for heroku

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express') , routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
@@ -31,6 +30,8 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
